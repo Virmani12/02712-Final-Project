fe02cc31279a537c23d78d17bab161bc48a3e1b8
Aman Virmani <virmania12@gmail.com>
2023-11-15 13:59:56 -0500

    finished init and alpha sims

--- initialization.py
+++ initialization.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 class Location:
 
@@ -27,34 +28,72 @@ class Grid:
 
         #Setting up initial population sizes per location, providing number of total locations, and index of each location
         n = self.n
+        #Random distribution of fractions to determine location population sizes (to prevent skewness)
+        rand_distr = np.random.dirichlet(np.ones(n_locations),size=1)
+        pop_sum = 0
+
+        #for each location, set its population size to be rand_fraction*N, 
+        # where the last location is just the difference between n and the sum of the rest of the population sizes
+        # this is done to prevent rounding errors from exceeding the total population size
         for i in range(n_locations):
-            rand_n = random.randint(0,n)
-            self.map.append(Location(rand_n, self.n_locations, i))
-            n -= rand_n
-    
+            if i == n_locations-1:
+                loc_size = n - pop_sum
+                self.map.append(Location(loc_size, self.n_locations, i))
+            else:
+                loc_size = int(rand_distr[0][i]*n)
+                self.map.append(Location(loc_size, self.n_locations, i))
+                pop_sum += loc_size
+            
         self.setup_connections()
-        self.random_orgin()
 
     def setup_connections(self):
-
 
         #For each location, get random split of connections to non-connections
         # For each list of connections, assign random "c" value for that connection between 0 and the population size of j
         for loc in self.map:
-
+            valid_locs = []
+            for other_loc in self.map:
+                if other_loc.ind != loc.ind:
+                    if other_loc.n > 0:
+                        valid_locs.append(other_loc)
+        
             split = random.uniform(0,1)
 
-            print("split:",split)
-            j_connections = random.sample(loc.connections.keys(), int(split*(self.n_locations-1)))
+            #print("split:",split)
+            j_connections = random.sample(valid_locs, int(split*len(valid_locs)))
 
             for j in j_connections:
-                loc.connections[j] = random.randint(1,self.map[j].n)
+                #print("Size: ",self.map[j.ind].n)
+                if self.map[j.ind].n == 1:
+                    loc.connections[j.ind] = 1
+                else:
+                    loc.connections[j.ind] = random.randint(1,self.map[j.ind].n)
         
     #This function sets a random starting point for the pandemic by assigning its I value to 1 and decremening its S value
+    #MAY NOT BE JUST INCREMENTING BY 1
     def random_orgin(self):
 
-        rand_start = random.randint(0,self.n_locations)
+        rand_start = random.randint(0,self.n_locations-1)
         self.map[rand_start].i = 1
         self.map[rand_start].s -= 1
+
+    def mobility_based_origin(self, threshold, highly):
+        #This function takes the top X-percentile of highly or lowly connected regions (based on total number of outgoing connections) 
+        # and sets their starting infection value to 1 while decreasing their susceptible by 1
+            #highly = True if getting top X-percentile of highly connected locations, False if getting top X-percentile of lowly connected locations
+            #threshold = top X-percentile to set as origin
+
+        connection_count = {}
+
+        for loc in self.map:
+            connection_count[loc.ind] = sum(loc.connections.values())
+
+        sorted_counts = sorted(connection_count.items(), key=lambda x: x[1], reverse=highly)
+        threshold = int(len(sorted_counts)*threshold)
+        top_keys = [item[0] for item in sorted_counts[:threshold]]
+
+        for key in top_keys:
+            self.map[key].i +=1
+            self.map[key].s -=1
 
 
